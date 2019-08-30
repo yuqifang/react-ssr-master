6438978589f3edebb36536b047282ca68fba3744
qifang.yu <1234.com>
2019-08-30 16:05:08 +0800

    学习理解

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 import path from 'path';
 import Koa from 'koa';
-import Cors from 'koa-cors';
+import Cors from 'koa-cors'; // 目的是解决跨域的问题
 import BodyParser from 'koa-bodyparser';
 import serve from 'koa-static';
-import Convert from 'koa-convert';
+import Convert from 'koa-convert'; // koa-convert最主要的作用是：将koa1包中使用的Generator函数准换成Koa2中的async函数
 import routes from './routes';
 import Config from './config';
 import templating from './templating';
@@ -13,7 +13,7 @@ import catchError from './middlewares/catchError';
 const app = new Koa();
 
 // 中间件处理
-app.use(Convert(Cors()));
+app.use(Convert(Cors())); // app.use(cors())的目的是允许跨域
 app.use(BodyParser());
 app.use(catchError());
 app.use(timeLogger());
